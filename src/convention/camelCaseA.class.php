add8fd7d80a10c6f0bf01d3f5727e10035508745
Matthew "Juniper" Barlett <emeraldinspirations@gmail.com>
2016-08-26 03:16:47 -0500

    Add convention camelCaseA

--- src/convention/camelCaseA.class.php
+++ src/convention/camelCaseA.class.php
@@ -9,7 +9,19 @@ class camelCaseA {
   use tConvention;
   
   public function toString() {
+    $pReturn    = '';
+    $pFirstWord = new firstIterationIdentifier();
     
+    foreach($this->_WordArray as $pWord) {
+      if($pFirstWord->isFirst( TRUE )) {
+        $pReturn  .= strtolower($pWord);
+      } else {
+        $pReturn  .=  strtoupper(substr($pWord, 0, 1))
+                  .   strtolower(substr($pWord, 1));
+      }
+    }
+    
+    return $pReturn;
   }
 
 }
